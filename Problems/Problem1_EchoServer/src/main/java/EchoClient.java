fda5b1b9cc06817e336368b169ba605c29b659b4
Daanan Dasilva <daanandasilva@gmail.com>
2023-10-31 13:30:48 -0600

    Problem 1 extended to allow multithreading and JavaDocs updated

--- Problems/Problem1_EchoServer/src/main/java/EchoClient.java
+++ Problems/Problem1_EchoServer/src/main/java/EchoClient.java
@@ -6,57 +6,81 @@ import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 
 
+/**
+ * Represents an echo client, which connects to a server and sends messages to it.
+ * The server responds by echoing the messages back to the client.
+ * The client then displays the echoed messages.
+ */
 public class EchoClient {
 
-
+    /**
+     * Entry point for the EchoClient application.
+     *
+     * @param args Command-line arguments (currently unused).
+     */
     public static void main(String[] args) {
+        // Hostname of the server to connect to.
         String hostName = "localhost";
-
+        // Port number on which the server is listening.
         int portNumber = 5555;
 
         try (
+                // Establish a socket connection to the server.
                 Socket echoSocket = new Socket(hostName, portNumber);
 
+                // Stream to send data to the server.
                 DataOutputStream out = new DataOutputStream(echoSocket.getOutputStream());
 
+                // Stream to receive data from the server.
                 DataInputStream in = new DataInputStream(echoSocket.getInputStream());
 
+                // Reader to get user input from the console.
                 BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in))
         ) {
+            // Display connection details.
             System.out.println("Server started and connected to client through: " +
                     "\nHostAddress: " + hostName +
                     "\nPortNumber: " + portNumber);
+
             while (true) {
+                // Read user input.
                 String userInput = stdIn.readLine();
+                // If user input is empty, skip the current loop iteration.
                 if (userInput.isEmpty()) {
                     continue;
                 }
 
+                // Convert user input to bytes and send to server.
                 byte[] bytes = userInput.getBytes(StandardCharsets.UTF_8);
                 out.writeInt(bytes.length);
                 out.write(bytes);
 
+                // Receive the echoed message length from the server.
                 int length = in.readInt();
-
+                // Validate the echoed message length.
                 if (length < 0 || length > 100_000) {
                     System.out.println("Invalid message length received");
                     return;
                 }
 
+                // Receive the actual echoed message.
                 byte[] responseBytes = new byte[length];
                 in.readFully(responseBytes);
                 String echoedMessage = new String(responseBytes, StandardCharsets.UTF_8);
 
+                // Display the echoed message.
                 System.out.println("Received from server: " + echoedMessage);
             }
         } catch (UnknownHostException e) {
+            // Handle errors related to unknown hosts.
             System.err.println("Don't know about host " + hostName);
             System.exit(1);
         } catch (IOException e) {
+            // Handle generic I/O exceptions.
             System.err.println("Couldn't get I/O for the connection to " + hostName);
             System.exit(1);
         }
     }
-
 }
+
 
--- Problems/Problem1_EchoServer/src/main/java/EchoServer.java
+++ Problems/Problem1_EchoServer/src/main/java/EchoServer.java
@@ -1,51 +1,184 @@
 package Problems.Problem1_EchoServer.src.main.java;
 
-import java.io.*;
-import java.net.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 
 public class EchoServer {
+    /**
+     * The port number on which the server listens.
+     */
     private static final int PORT_NUMBER = 5555;
+    /**
+     * Maximum allowed message length in bytes.
+     */
     private static final int MAX_MESSAGE_LENGTH = 100_000;
-    private static final int SOCKET_TIMEOUT = 10_000;
+    /**
+     * Socket timeout in milliseconds.
+     */
+    private static final int SOCKET_TIMEOUT = 60_000;
+
+    /**
+     * Entry point for the EchoServer application.
+     * <p>
+     * This method initializes the EchoServer, setting up a server socket that listens on a
+     * predefined port. The server utilizes a ThreadPoolExecutor to handle incoming client
+     * connections concurrently, improving scalability and efficiency.
+     * </p>
+     * <p>
+     * Key operations performed within this method:
+     * <ul>
+     *     <li>Initialize a ServerSocket on a predefined port.</li>
+     *     <li>Set up a ThreadPoolExecutor with specified core, maximum pool sizes, keep-alive time,
+     *     and a work queue.</li>
+     *     <li>Add a shutdown hook to ensure the executor service is gracefully shut down on
+     *     application termination.</li>
+     *     <li>Continuously listen for and accept incoming client connections, handing each
+     *     off to the executor service.</li>
+     * </ul>
+     * </p>
+     * <p>
+     * Any IOException encountered during the server socket setup or client communication is
+     * caught and wrapped in a RuntimeException.
+     * </p>
+     *
+     * @param args command-line arguments (not used in this application).
+     */
     public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(EchoServer.PORT_NUMBER)) {
+            // Initialization and display of server details.
+            System.out.println("Server started on: " +
+                    "\nHostAddress: " + serverSocket.getInetAddress() +
+                    "\nPortNumber: " + PORT_NUMBER);
 
+            // ThreadPoolExecutor setup with specified parameters.
+            int corePoolSize = 5;
+            int maximumPoolSize = 50;
+            long keepAliveTime = 60L;
+            TimeUnit unit = TimeUnit.SECONDS;
+            BlockingQueue<Runnable> workQueue = new LinkedBlockingQueue<>(1000);
+            ThreadPoolExecutor executorService = new ThreadPoolExecutor(
+                    corePoolSize,
+                    maximumPoolSize,
+                    keepAliveTime,
+                    unit,
+                    workQueue
+            );
 
-        try (
-                ServerSocket serverSocket = new ServerSocket(EchoServer.PORT_NUMBER);
+            // Adding a shutdown hook to gracefully shutdown the executor service.
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Shutting down executor...");
+                executorService.shutdown();
+                try {
+                    if (!executorService.awaitTermination(60, TimeUnit.SECONDS)) {
+                        executorService.shutdownNow();
+                    }
+                } catch (InterruptedException e) {
+                    executorService.shutdownNow();
+                    e.printStackTrace();
+                }
+                System.out.println("Executor shutdown complete");
+            }));
 
+            // Continuously accept incoming client connections and handle using the executor service.
+            while (true) {
                 Socket clientSocket = serverSocket.accept();
+                clientSocket.setSoTimeout(EchoServer.SOCKET_TIMEOUT);
+                executorService.execute(() -> handleClient(clientSocket));
+            }
 
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * Handles communication with a connected client.
+     *
+     * <p>
+     * This method is responsible for:
+     * <ul>
+     *     <li>Setting up input and output streams for communication with the client.</li>
+     *     <li>Receiving messages from the client and echoing them back. The messages are expected
+     *     to be preceded by an integer indicating the message length.</li>
+     *     <li>Ensuring that received messages are within valid length bounds.</li>
+     *     <li>Gracefully handling various exceptions, such as socket exceptions or abrupt client disconnections.</li>
+     * </ul>
+     * </p>
+     *
+     * <p>
+     * The communication protocol is as follows:
+     * <ol>
+     *     <li>Read an integer which indicates the length of the incoming message.</li>
+     *     <li>If the length is invalid (either negative or exceeds the maximum allowed length),
+     *     terminate the communication.</li>
+     *     <li>Read the message bytes based on the received length.</li>
+     *     <li>Convert the bytes to a string and display the received message.</li>
+     *     <li>Echo the message back to the client.</li>
+     * </ol>
+     * </p>
+     *
+     * @param clientSocket The socket through which the server communicates with the connected client.
+     */
+    private static void handleClient(Socket clientSocket) {
+        try (
+                // Create output stream to send data to the client
                 DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
 
+                // Create input stream to receive data from the client
                 DataInputStream in = new DataInputStream(clientSocket.getInputStream())
         ) {
-            System.out.println("Server started and connected to client through: " +
-                    "\nHostAddress: " + serverSocket.getInetAddress().getCanonicalHostName() +
+            // Log the connection details for debugging purposes
+            System.out.println("Server connected to client through: " +
+                    "\nHostAddress: " + clientSocket.getInetAddress() +
                     "\nPortNumber: " + EchoServer.PORT_NUMBER);
 
             while (true) {
+                // Receive the length of the incoming message
                 int length = in.readInt();
-                if (length < 0 || length > 100_000) {
+
+                // Check the validity of the received message length
+                if (length < 0 || length > EchoServer.MAX_MESSAGE_LENGTH) {
                     System.out.println("Invalid message length received");
                     return;
                 }
+
+                // Read the actual message based on the provided length
                 byte[] bytes = new byte[length];
                 in.readFully(bytes);
 
+                // Convert the received bytes to a string for logging and processing
                 String receivedMessage = new String(bytes, StandardCharsets.UTF_8);
                 System.out.println("Received from client: " + receivedMessage);
 
+                // Echo the received message back to the client
                 out.writeInt(length);
                 out.write(bytes);
             }
-        } catch (
-                IOException e) {
-            System.out.println("Exception caught when trying to listen on port " + EchoServer.PORT_NUMBER
-                    + " or listening for a connection");
+        } catch (SocketException e) {
+            // Handle socket-related exceptions (e.g., connection reset or connection aborted)
+            System.out.println("Socket error on port " + EchoServer.PORT_NUMBER);
+            System.out.println(e.getMessage());
+        } catch (EOFException e) {
+            // Handle cases where the client disconnects abruptly without a proper closure
+            System.out.println("Client disconnected abruptly.");
+        } catch (IOException e) {
+            // Handle generic I/O exceptions
+            System.out.println("Exception caught when trying to communicate on port " + EchoServer.PORT_NUMBER);
             System.out.println(e.getMessage());
         }
     }
+
 }
 
